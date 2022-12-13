da4d0fc5a53e8083504cf9984686b7e3ef30b78d
nazart88 <parlato.developing@gmail.com>
2022-12-13 10:27:30 +0100

    Upgrade_Nazareno

--- Progetto_Secondo/force-app/main/default/lwc/lightprova/lightprova.js
+++ Progetto_Secondo/force-app/main/default/lwc/lightprova/lightprova.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class Lightprova extends LightningElement {}
+export default class Lightprova extends LightningElement {
+    greeting = 'World';
+    changeHandler(event) {
+      this.greeting = event.target.value;
+    }
+}
