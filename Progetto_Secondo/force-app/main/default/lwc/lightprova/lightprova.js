6566a7f69dbfbde13b7d611e5287ee73554a94d9
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2022-12-18 10:09:32 +0100

    add front-end functionality

--- Progetto_Secondo/force-app/main/default/lwc/lightprova/lightprova.js
+++ Progetto_Secondo/force-app/main/default/lwc/lightprova/lightprova.js
@@ -1,21 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import createOpp from '@salesforce/apex/CreateOpportunity.createOpp';
+import retrieveOpp from '@salesforce/apex/CreateOpportunity.retrieveOpp';
 
 export default class Lightprova extends LightningElement {
-  stage;  
-  options = [
-    {value: "Prospecting", label: "Prospecting"},
-    {value: "Qualification", label: "Qualification"},
-    {value: "Needs Analysis", label: "Needs Analysis"},
-    {value: "Value Proposition", label: "Value Proposition"},
-    {value: "Id. Decision Makers", label: "Id. Decision Makers"},
-    {value: "Perception Analysis", label: "Perception Analysis"},
-    {value: "Proposal/Price Quote", label: "Proposal/Price Quote"},
-    {value: "Negotiation/Review", label: "Negotiation/Review"},
-    {value: "Closed Won", label: "Closed Lost"}
-  ];
+  isDataVisible = false;
+  get options() {
+    return [
+      {value: null, label: "--None--"},
+      {value: "Prospecting", label: "Prospecting"},
+      {value: "Qualification", label: "Qualification"},
+      {value: "Needs Analysis", label: "Needs Analysis"},
+      {value: "Value Proposition", label: "Value Proposition"},
+      {value: "Id. Decision Makers", label: "Id. Decision Makers"},
+      {value: "Perception Analysis", label: "Perception Analysis"},
+      {value: "Proposal/Price Quote", label: "Proposal/Price Quote"},
+      {value: "Negotiation/Review", label: "Negotiation/Review"},
+      {value: "Closed Won", label: "Closed Lost"}
+    ];
+  } 
+
+  @track
+  opportunityFields = {
+    name: "",
+    closeDate: new Date(),
+    stage: this.options[0].value
+  };  
+
+  handleClick() {
+    this.isDataVisible = !this.isDataVisible;
+  }
+
+  handleNameChange(e) {
+    this.opportunityFields = {...this.opportunityFields, name: e.target.value};
+  }
+
+  handleDateChange(e) {
+    this.opportunityFields = {...this.opportunityFields, closeDate: e.target.value};
+  }
 
   handleStageChange(e) {
-    this.stage = e.target.value;
-    console.log(this.stage);
+    this.opportunityFields = {...this.opportunityFields, stage: e.detail.value};
   }
 }
