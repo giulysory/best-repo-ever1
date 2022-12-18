f454984ea51aad6e10d5b745b0b88206190046da
Graziella Romeo <graziellaromeo2@gmail.com>
2022-12-18 23:33:45 +0100

    Created apex class ContactCreator and used its method on the lwc

--- Progetto_Secondo/force-app/main/default/lwc/lightform/lightform.js
+++ Progetto_Secondo/force-app/main/default/lwc/lightform/lightform.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
+import createContact from '@salesforce/apex/ContactCreator.createContact';
 
-export default class Lightform extends LightningElement {}
+export default class NewContactForm extends LightningElement {
+  name;
+  lastName;
+  phone;
+
+  handleNameChange(event) {
+    this.name = event.target.value;
+  }
+
+  handleLastNameChange(event) {
+    this.lastName = event.target.value;
+  }
+
+  handlePhoneChange(event) {
+    this.phone = event.target.value;
+  }
+
+  handleSubmit() {
+    createContact(name, lastName, phone)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+}
